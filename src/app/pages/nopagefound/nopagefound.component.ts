4585e6498770876378ceb45780b3351fc01bf29f
Enrique Tun <enrique.tun@therocketcode.com>
2024-07-24 18:10:33 -0600

    feat: se agrego la pagina de 404

--- src/app/pages/nopagefound/nopagefound.component.ts
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styles: ``
+  styleUrls: ['./nopagefound.component.css']
 })
 export class NopagefoundComponent {
 
