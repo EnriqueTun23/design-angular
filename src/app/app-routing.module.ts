3062a3623eff6859855b41a5b745ceac326ebe65
Enrique Tun <enrique.tun@therocketcode.com>
2024-07-30 13:51:20 -0600

    feat: se agrego el el temal de refactor para las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PageRoutingModule } from './pages/pages.routing';
+import { AuthRoutingsModule } from './auth/auth.routing';
 
 const routes: Routes = [
-
-
-
-  {path: 'login', component: LoginComponent },
-  {path: 'register', component: RegisterComponent },
-
-
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {path: '**', component: NopagefoundComponent }
 ]
 @NgModule({
   declarations: [],
   imports:[
    RouterModule.forRoot(routes),
-   PageRoutingModule
+   PageRoutingModule,
+   AuthRoutingsModule
   ],
   exports: [RouterModule]
 })
